dabc3e32c1cb2243b848b109acd9165a41ffc276
Shivam Sharma <shivam.piyush99@gmail.com>
2022-03-13 18:59:56 +0530

    ASP .NET program in MVC 5

---
+++ Models/Classmodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Student.Models
+{
+	public class Classmodel
+	{
+		public string name { get; set; }
+		public int rollno { get; set; }
+		public string email { get; set; }
+	}
+}
